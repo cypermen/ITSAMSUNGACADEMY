aef215070ce0086f47bf3ed251458db8b1ab8302
cypermen <andreybabich2012@yandex.ru>
2019-11-10 01:32:16 +0300

    1.8.7 The simplest animation
    Movement of the black circle from left to right

--- app/src/main/java/com/example/itsamsungacademy/MyView.java
+++ app/src/main/java/com/example/itsamsungacademy/MyView.java
@@ -12,38 +12,18 @@ public class MyView extends View {
         super(context);
     }
 
+    Paint paint = new Paint();
+    float x = 0;
+
+    long lastTime = System.currentTimeMillis();
     @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        Paint paint = new Paint();
-        Paint paint1 = new Paint();
-        paint.setColor(Color.RED);
-        paint1.setColor(Color.BLUE);
-        paint.setStrokeWidth(10);
-        paint1.setStrokeWidth(10);
+    protected void onDraw(Canvas canvas){
+        canvas.drawCircle(x, 300, 20, paint);
 
-        //red rhombuses
-        canvas.drawLine(canvas.getWidth()*1.0f / 4,0,0,canvas.getHeight()*1.0f / 4,paint);
-        canvas.drawLine(0,canvas.getHeight()*1.0f / 4,canvas.getWidth()*1.0f*(3*1.0f/4),canvas.getHeight(),paint);
-        canvas.drawLine(canvas.getWidth()*1.0f / 4,0,canvas.getWidth(),canvas.getHeight()*1.0f*(3*1.0f/4),paint);
-        canvas.drawLine(canvas.getWidth(),canvas.getHeight()*1.0f*(3*1.0f / 4),canvas.getWidth()*1.0f*(3*1.0f / 4),canvas.getHeight(),paint);
-        canvas.drawLine(canvas.getWidth()*1.0f * (3*1.0f / 4),0,0,canvas.getHeight()*1.0f * (3*1.0f / 4),paint);
-        canvas.drawLine(canvas.getWidth()*1.0f * (3*1.0f / 4),0,canvas.getWidth(),canvas.getHeight()*1.0f / 4,paint);
-        canvas.drawLine(canvas.getWidth(),canvas.getHeight()*1.0f / 4 ,canvas.getWidth()*1.0f / 4 ,canvas.getHeight(),paint);
-        canvas.drawLine(0,canvas.getHeight()*1.0f * (3*1.0f / 4),canvas.getWidth()*1.0f / 4,canvas.getHeight(),paint);
+        long nowTime = System.currentTimeMillis();
+        x += 0.01f * (nowTime - lastTime);
 
-
-        //blue rhombuses
-        canvas.drawLine(canvas.getWidth()*1.0f / 2 ,0,canvas.getWidth(),canvas.getHeight()*1.0f / 2,paint1);
-        canvas.drawLine(canvas.getWidth()*1.f / 2 ,0,0,canvas.getHeight()*1.0f / 2,paint1);
-        canvas.drawLine(canvas.getWidth()*1.0f / 2,canvas.getHeight(),0,canvas.getHeight()*1.0f / 2,paint1);
-        canvas.drawLine(canvas.getWidth()*1.0f / 2,canvas.getHeight(),canvas.getWidth(),canvas.getHeight()*1.0f / 2,paint1);
-        canvas.drawLine(canvas.getWidth()*1.0f / 4,canvas.getHeight()*1.0f / 4,canvas.getWidth()*1.0f * (3*1.0f / 4),canvas.getHeight()*1.0f * (3*1.0f / 4),paint1);
-        canvas.drawLine(getWidth()*0.75f,getHeight()*0.25f,getWidth()*0.25f,getHeight()*0.75f,paint1);
-
-
-
-
-
+        lastTime = nowTime;
+        invalidate();
     }
 }
