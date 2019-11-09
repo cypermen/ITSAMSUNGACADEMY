08820eec4f8c5b05353c91d2af0d03b2c374e333
cypermen <andreybabich2012@yandex.ru>
2019-11-10 02:34:55 +0300

    1.8.11 Improved program

--- app/src/main/java/com/example/itsamsungacademy/MyView.java
+++ app/src/main/java/com/example/itsamsungacademy/MyView.java
@@ -4,88 +4,84 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.os.CountDownTimer;
 import android.view.View;
 
-
 public class MyView extends View {
+    Paint paint = new Paint();
+    int N = 10;
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    int[] red = new int[N];
+    int[] green = new int[N];
+    int[] blue = new int[N];
+    float[] radius = new float[N];
 
-    int N = 15;
-    int[] l = new int [N];
-    double x0, y0;
-    double[] x = new double [N];
-    double[] y = new double [N];
-    double g = 9.832f, pi = Math.PI;
-    double[] w = new double[N];
-    double fi0;
-    double[] fi = new double[N];
-    int t = 0, deltaT = 1;
-
-    void makePendulum()
-    {
-        fi0 = pi/4;
-
-        int l_min = 100;
-        for (int i = 0; i<N; i++)
-        {
-            l[i] = l_min;
-            l_min += 50;
-
-            w[i] = Math.sqrt(g/l[i]);
-        }
-    }
-    void movePendulum()
-    {
-        t += deltaT;
-
-        for (int i = 0; i<N; i++)
-        {
-            fi[i] = fi0 * Math.cos(w[i] * t);
-            x[i] = l[i]*Math.sin(fi[i]);
-            y[i] = l[i]*Math.cos(fi[i]);
-        }
-    }
-
-    MyView(Context context) {
+    public MyView(Context context) {
         super(context);
-        makePendulum();
-        MyTimer timer = new MyTimer();
-        timer.start();
     }
 
+
+
+    boolean started = false;
     @Override
-    protected void onDraw(Canvas canvas) {
-        x0 = getWidth()/2;
-        y0 = getHeight()/4;
-        Paint paint = new Paint();
-        canvas.drawCircle((float) x0, (float) y0, 10, paint);
-        for (int i = 0; i<N; i++)
-        {
-            paint.setColor(Color.BLUE);
-            canvas.drawLine((float)x0, (float)y0, (float)(x[i] + x0), (float)(y[i]+ y0), paint);
-            paint.setColor(Color.RED);
-            canvas.drawCircle((float)(x[i] + x0), (float)(y[i] + y0), 20, paint);
-        }
-    }
+    protected void onDraw(Canvas canvas){
+        if (!started){
+            for (int i = 0; i < N; i++){
+                x[i] = (float)(Math.random() * getWidth());
+                y[i] = (float)(Math.random() * getHeight());
+                vx[i] = (float)(Math.random() * 6 - 3);
+                vy[i] = (float)(Math.random() * 6 - 3);
+                red[i] = (int) (Math.random()*255);
+                green[i] = (int) (Math.random()*255);
+                blue[i] = (int) (Math.random()*255);
+                radius[i]=(float)(Math.random()*50);
 
-    void nextFrame()
-    {
-        movePendulum();
+            }
+            started = true;
+        }
+        for (int i = 0; i < N; i++) {
+            if (x[i] < 0 || x[i] > this.getHeight()){
+                paint.setColor(Color.rgb(red[i],green[i],blue[i]));
+            }
+            if (y[i] < 0 || y[i] > this.getHeight()){
+                paint.setColor(Color.rgb(red[i],green[i],blue[i]));
+
+            }
+            canvas.drawCircle(x[i], y[i], radius[i], paint);
+        }
+        for(int i = 0;i < N;i++){
+            for(int j = i + 1;j < N;j++){
+                if(Math.sqrt((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]))<=2*radius[i]){
+                    if( radius[i] > radius[j] ) {
+                        vx[j] = vx[i] * (radius[i] * 1.0f / radius[j]);
+                        vy[j] = vy[i] * (radius[i] * 1.0f / radius[j]);
+                    }else if(radius[i] == radius[j]){
+                        vx[i]=-vx[i];
+                        vy[i]=-vx[i];
+                        vx[j]=-vx[j];
+                        vy[j]=-vx[j];
+                    }else{
+                        vx[i] = vx[j] * (radius[j] * 1.0f / radius[i]);
+                        vy[i] = vy[j] * (radius[j] * 1.0f / radius[i]);
+                    }
+
+                }
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            x[i] += vx[i];
+            y[i] += vy[i];
+            if (x[i] < 0 || x[i] > this.getHeight()){
+                vx[i] = - vx[i];
+            }
+            if (y[i] < 0 || y[i] > this.getHeight()){
+                vy[i] = - vy[i];
+
+            }
+        }
+
         invalidate();
-    }
-
-    class MyTimer extends CountDownTimer
-    {
-        MyTimer()
-        {
-            super(100000, 100);
-        }
-        @Override
-        public void onTick(long millisUntilFinished) {
-            nextFrame();
-        }
-        @Override
-        public void onFinish() {
-        }
     }
 }
