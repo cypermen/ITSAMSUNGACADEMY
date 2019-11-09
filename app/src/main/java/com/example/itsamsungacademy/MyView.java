d276a9d7fe41ec962bc85210aae05c38d1358b46
cypermen <andreybabich2012@yandex.ru>
2019-11-09 17:28:53 +0300

    1.7.6 Drawing patterns on Canvas
    Drawing Circle

--- app/src/main/java/com/example/itsamsungacademy/MyView.java
+++ app/src/main/java/com/example/itsamsungacademy/MyView.java
@@ -16,12 +16,14 @@ public class MyView extends View {
     protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            Paint paint = new Paint();
-
-            paint.setColor(Color.GREEN);
-            paint.setStrokeWidth(5);
-
-            canvas.drawLine(0,0,getWidth(),getHeight(),paint);
+        Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(300, 300, 200, paint);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(20);
+        canvas.drawCircle(300, 300, 200, paint);
 
     }
 }
